6673d3da09a949217bc0357a7d99659c7a533cb8
Ubuntu <ubuntu@ip-172-31-32-89.eu-north-1.compute.internal>
2026-06-02 17:46:12 +0000

    Added menu

--- monitor.py
+++ monitor.py
@@ -1,12 +1,22 @@
 import psutil
-print("------------------Server Health Dashboard-------------------")
 
-cpu=psutil.cpu_percent(interval=1)
-memory=psutil.virtual_memory().percent
-disk=psutil.disk_usage('/').percent
+print("----Menu:----")
+print("1.Check Server Health")
+print("2.Exit")
 
-print(f"CPU Usage:{cpu}%")
-print(f"Memory Usage:{memory}%")
-print(f"Disk Usage:{disk}%")
+choice=int(input("Enter your choice: "))
 
-print("-------------------------------------------------------------")
+if choice==1:
+    print("------------------Server Health Dashboard-------------------")
+    cpu=psutil.cpu_percent(interval=1)
+    memory=psutil.virtual_memory().percent
+    disk=psutil.disk_usage('/').percent
+    print(f"CPU Usage:{cpu}%")
+    print(f"Memory Usage:{memory}%")
+    print(f"Disk Usage:{disk}%")
+    print("-------------------------------------------------------------")
+elif choice==2:
+    print("Exiting the program...")
+else:
+    print('Invalid Choice"')
+    exit()
